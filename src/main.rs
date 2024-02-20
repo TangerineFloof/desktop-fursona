3bdc5fb1247be99c2a8332f98f3afc70d18c3b37
Jack <jack@tangerinefloof.com>
2024-02-19 21:39:29 -0800

    Stop printing every event in event loop

--- src/main.rs
+++ src/main.rs
@@ -28,43 +28,40 @@ fn main() -> Result<(), impl std::error::Error> {
     let device_state = DeviceState::checked_new().unwrap();
     let mut prev_mouse_state = MouseState::default();
 
-    event_loop.run(move |event, elwt| {
-        println!("{event:?}");
-        match event {
-            Event::NewEvents(StartCause::Poll) => {
-                if device_state.get_keys().contains(&Keycode::Escape) {
-                    elwt.exit();
-                }
-
-                let mouse_state = device_state.get_mouse();
-                if mouse_state != prev_mouse_state {
-                    stage.on_mouse_over(mouse_state.coords.0 as u32, mouse_state.coords.1 as u32);
-
-                    let lmb = mouse_state.button_pressed.get(1).unwrap_or(&false);
-                    if *lmb {
-                        println!("LMB click: {:?}", mouse_state.coords);
-                    }
-
-                    prev_mouse_state = mouse_state;
-                }
+    event_loop.run(move |event, elwt| match event {
+        Event::NewEvents(StartCause::Poll) => {
+            if device_state.get_keys().contains(&Keycode::Escape) {
+                elwt.exit();
             }
-            Event::Resumed => {
-                let jack = Box::new(Renderer2D::new(&stage, "./jack_by_nal_cinnamonspots.png"));
-                renderers.push(jack);
+
+            let mouse_state = device_state.get_mouse();
+            if mouse_state != prev_mouse_state {
+                stage.on_mouse_over(mouse_state.coords.0 as u32, mouse_state.coords.1 as u32);
+
+                let lmb = mouse_state.button_pressed.get(1).unwrap_or(&false);
+                if *lmb {
+                    println!("LMB click: {:?}", mouse_state.coords);
+                }
+
+                prev_mouse_state = mouse_state;
             }
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::Resized(size) => {
-                    if size.width != 0 && size.height != 0 {
-                        stage.resize(size.width, size.height);
-                    }
-                }
-                WindowEvent::RedrawRequested => {
-                    stage.draw(&renderers);
-                }
-                WindowEvent::CloseRequested => elwt.exit(),
-                _ => (),
-            },
-            _ => (),
         }
+        Event::Resumed => {
+            let jack = Box::new(Renderer2D::new(&stage, "./jack_by_nal_cinnamonspots.png"));
+            renderers.push(jack);
+        }
+        Event::WindowEvent { event, .. } => match event {
+            WindowEvent::Resized(size) => {
+                if size.width != 0 && size.height != 0 {
+                    stage.resize(size.width, size.height);
+                }
+            }
+            WindowEvent::RedrawRequested => {
+                stage.draw(&renderers);
+            }
+            WindowEvent::CloseRequested => elwt.exit(),
+            _ => (),
+        },
+        _ => (),
     })
 }
